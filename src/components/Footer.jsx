9ed3ad6b600a17989e9df0f0c849c49af39693c7
Gerald Imanuel <gerald.imanuel@student.umn.ac.id>
2023-07-28 16:37:28 +0700

    fix: fix copywriting data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,18 +24,25 @@ export default function Footer() {
 						Jalan Let Jendral Suprapto No.128
 					</Text>
 				</Flex>
-				<Flex className="phone" alignItems="center" gap={3}>
-					<MdAddIcCall size="25px" />
-					<Text fontSize="sm" fontWeight="semibold">
-						0812 2147 8090
-					</Text>
-				</Flex>
-				<Flex className="email" alignItems="center" gap={3}>
-					<HiMailOpen size="25px" />
-					<Text fontSize="sm" fontWeight="semibold">
-						piasuper@gmail.com
-					</Text>
-				</Flex>
+				<a href="https://wa.me/+6281224998556">
+					<Flex className="phone" alignItems="center" gap={3}>
+						<MdAddIcCall size="25px" />
+						<Text fontSize="sm" fontWeight="semibold">
+							+ 62 812 2499 8556
+						</Text>
+					</Flex>
+				</a>
+				<a
+					href="
+				https://mail.google.com/mail/?view=cm&to=dlanovdelights@gmail.com&su=[...]&body=..."
+				>
+					<Flex className="email" alignItems="center" gap={3}>
+						<HiMailOpen size="25px" />
+						<Text fontSize="sm" fontWeight="semibold">
+							dlanovdelights@gmail.com
+						</Text>
+					</Flex>
+				</a>
 			</Flex>
 			<SimpleGrid className="navbar-mini" columns={3} spacing={3}>
 				<Text fontSize="sm" textAlign={{ base: "center", md: "left" }}>
